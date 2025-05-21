cf2f4999f357feccc1ac49b73ef85d7212c20a8c
Christian Rock <rockcm@etsu.edu>
2025-05-21 12:09:30 -0400

    changed test description

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,48 +1,48 @@
-const http = require('http');
-const { expect } = require('chai');
-const app = require('../main');
+const http = require('http'); // Import the http module to make HTTP requests
+const { expect } = require('chai'); // Import the expect function from Chai for assertions
+const app = require('../main'); // Import the Express app from main.js
 
-let server;
+let server; // Variable to hold the server instance
 
 // Start server before tests
 before(function (done) {
-  server = app.listen(3000, () => {
+  server = app.listen(3000, () => { // Start the server on port 3000
     console.log('Test server started on port 3000');
-    done();
+    done(); // Signal that the asynchronous setup is complete
   });
 });
 
 // Close server after tests
 after(function (done) {
-  server.close(() => {
+  server.close(() => { // Close the server after tests are done
     console.log('Test server closed');
-    done();
+    done(); // Signal that the asynchronous teardown is complete
   });
 });
 
-describe('Node.js API', () => {
-  it('GET / should return Hello, World!', (done) => {
-    http.get('http://localhost:3000/', (res) => {
-      let data = '';
+describe('Node.js API', () => { // Describe the test suite for the Node.js API
+  it('GET / should return Hello, Worlerted!', (done) => { // Test case for the root endpoint
+    http.get('http://localhost:3000/', (res) => { // Make a GET request to the root endpoint
+      let data = ''; // Variable to accumulate response data
       
       // Read response data
-      res.on('data', chunk => data += chunk);
-      res.on('end', () => {
-        const json = JSON.parse(data);
-        expect(json.message).to.equal('Hello, World!');
-        done();
+      res.on('data', chunk => data += chunk); // Append each chunk of data to the data variable
+      res.on('end', () => { // When the response ends
+        const json = JSON.parse(data); // Parse the response data as JSON
+        expect(json.message).to.equal('Hello, World!'); // Assert that the message is 'Hello, World!'
+        done(); // Signal that the test is complete
       });
     });
   });
 
-  it('GET /users should return an array', (done) => {
-    http.get('http://localhost:3000/users', (res) => {
-      let data = '';
-      res.on('data', chunk => data += chunk);
-      res.on('end', () => {
-        const json = JSON.parse(data);
-        expect(json).to.be.an('array');
-        done();
+  it('GET /users should return an array', (done) => { // Test case for the /users endpoint
+    http.get('http://localhost:3000/users', (res) => { // Make a GET request to the /users endpoint
+      let data = ''; // Variable to accumulate response data
+      res.on('data', chunk => data += chunk); // Append each chunk of data to the data variable
+      res.on('end', () => { // When the response ends
+        const json = JSON.parse(data); // Parse the response data as JSON
+        expect(json).to.be.an('array'); // Assert that the response is an array
+        done(); // Signal that the test is complete
       });
     });
   });
